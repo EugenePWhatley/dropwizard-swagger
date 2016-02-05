6907d430b4f8e03c65547ccf14a507eee508c6c6
EugenePWhatley <ewhatley@thoughtworks.com>
2016-02-05 15:13:19 -0600

    does adding in resource

--- src/main/groovy/HelloGuiceModule.groovy
+++ src/main/groovy/HelloGuiceModule.groovy
@@ -1,8 +1,5 @@
 import com.google.inject.AbstractModule
-import com.google.inject.Provides
 import io.dropwizard.setup.Environment
-import provider.FizzBuzzProvider
-
 
 class HelloGuiceModule extends AbstractModule {
     private final HelloConfiguration configuration
@@ -11,12 +8,6 @@ class HelloGuiceModule extends AbstractModule {
     HelloGuiceModule(final HelloConfiguration configuration, final Environment environment) {
         this.configuration = configuration
         this.environment = environment
-    }
-
-    @Provides
-    FizzBuzzProvider prepareFizzBuzzProvider() {
-        FizzBuzzProvider fizzBuzzProvider = new FizzBuzzProvider()
-        fizzBuzzProvider
     }
 
     @Override
--- src/main/groovy/resource/HelloWorldResource.groovy
+++ src/main/groovy/resource/HelloWorldResource.groovy
@@ -3,6 +3,7 @@ package resource
 import com.google.inject.Inject
 import com.wordnik.swagger.annotations.Api
 import com.wordnik.swagger.annotations.ApiOperation
+import provider.CalculatorProvider
 import provider.FizzBuzzProvider
 
 import javax.ws.rs.GET
@@ -16,19 +17,29 @@ import javax.ws.rs.core.MediaType
 @Produces(MediaType.APPLICATION_JSON)
 class HelloWorldResource {
 
-	@Inject
-	FizzBuzzProvider fizzBuzzProvider
+    @Inject
+    FizzBuzzProvider fizzBuzzProvider
 
-	@GET
-	@ApiOperation(value = 'list of all the hello world examples')
-	def helloWorldExamples() {
-		[helloWorldExamples: ['FizzBuzz']]
-	}
+    @Inject
+    CalculatorProvider calculatorProvider
 
-	@GET
-	@Path('/fizzBuzz')
-	@ApiOperation(value = 'counts up to n')
-	def fizzBuzz(@QueryParam('countTo') int countTo) {
-		[countUpTo: countTo, counter: fizzBuzzProvider.count(countTo)]
-	}
+    @GET
+    @ApiOperation(value = 'list of all the hello world examples')
+    def helloWorldExamples() {
+        [helloWorldExamples: ['FizzBuzz']]
+    }
+
+    @GET
+    @Path('/fizzBuzz')
+    @ApiOperation(value = 'counts up to n')
+    def fizzBuzz(@QueryParam('countTo') int countTo) {
+        [countUpTo: countTo, counter: fizzBuzzProvider.count(countTo)]
+    }
+
+    @GET
+    @Path('/calculator')
+    @ApiOperation(value = 'adds two numbers')
+    def calculator(@QueryParam('x') double x, @QueryParam('y') double y) {
+        [operation: 'addition', values: x + ' and ' + y, answer: calculatorProvider.add(x, y)]
+    }
 }
