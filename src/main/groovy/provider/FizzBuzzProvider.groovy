964aea735c54af362d9534336729f9dd9d2b9d9f
EugenePWhatley <ewhatley@thoughtworks.com>
2016-02-05 14:27:22 -0600

    replaces 5 with buzz

--- src/main/groovy/provider/FizzBuzzProvider.groovy
+++ src/main/groovy/provider/FizzBuzzProvider.groovy
@@ -6,6 +6,8 @@ class FizzBuzzProvider {
         1.upto(i, {
             if (it == 3) {
                 counter.add('fizz')
+            } else if (it == 5) {
+                counter.add('buzz')
             } else {
                 counter.add(it)
             }
--- src/test/groovy/provider/FizzBuzzProviderTest.groovy
+++ src/test/groovy/provider/FizzBuzzProviderTest.groovy
@@ -3,36 +3,50 @@ package provider
 import spock.lang.Specification
 
 class FizzBuzzProviderTest extends Specification {
+    private FizzBuzzProvider fizzBuzzProvider
+
+    void setup() {
+        fizzBuzzProvider = new FizzBuzzProvider()
+    }
 
     def 'should create a counter provider class'() {
         when:
-        FizzBuzzProvider counter = new FizzBuzzProvider()
+        FizzBuzzProvider fizzBuzzProviderCreation = new FizzBuzzProvider()
 
         then:
-        counter != null
+        fizzBuzzProviderCreation != null
     }
 
     def 'should count up to 2'() {
         setup:
-        FizzBuzzProvider counter = new FizzBuzzProvider()
         def desiredCount = 2
 
         when:
-        def response = counter.count(desiredCount)
+        def response = fizzBuzzProvider.count(desiredCount)
 
         then:
-        response == [1,2]
+        response == [1, 2]
     }
 
     def 'should count up 4 and replace 3 with fizz'() {
         setup:
-        FizzBuzzProvider counter = new FizzBuzzProvider()
         def desiredCount = 4
 
         when:
-        def response = counter.count(desiredCount)
+        def response = fizzBuzzProvider.count(desiredCount)
 
         then:
-        response == [1,2,'fizz',4]
+        response == [1, 2, 'fizz', 4]
+    }
+
+    def 'should count up to 6 and replace 5 with buzz'() {
+        setup:
+        def desiredCount = 6
+
+        when:
+        def response = fizzBuzzProvider.count(desiredCount)
+
+        then:
+        response == [1, 2, 'fizz', 4, 'buzz', 6,]
     }
 }
